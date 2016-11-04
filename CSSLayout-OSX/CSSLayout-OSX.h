8f51922747ae1abad7ee320a95b15852e4c5f8a1
Jason Jobe <github@jasonjobe.com>
2016-11-03 20:30:04 -0400

    added OSX target

---
+++ CSSLayout-OSX/CSSLayout-OSX.h
@@ -0,0 +1,19 @@
+//
+//  CSSLayout-OSX.h
+//  CSSLayout-OSX
+//
+//  Created by Jason Jobe on 10/13/16.
+//  Copyright © 2016 Matias Cudich. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CSSLayout-OSX.
+FOUNDATION_EXPORT double CSSLayout_OSXVersionNumber;
+
+//! Project version string for CSSLayout-OSX.
+FOUNDATION_EXPORT const unsigned char CSSLayout_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSSLayout_OSX/PublicHeader.h>
+
+
